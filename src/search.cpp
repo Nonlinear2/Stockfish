edd3189b683a220dd89e332b54db23f397262410
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-08-03 11:02:35 +0200

    SEE pruning attempt

--- src/search.cpp
+++ src/search.cpp
@@ -1566,9 +1566,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
                 // If static eval is much lower than alpha and move is
                 // not winning material, we can prune this move. (~2 Elo)
-                if (!pos.see_ge(move, alpha - futilityBase))
+                if (!pos.see_ge(move, alpha - ss->staticEval))
                 {
-                    bestValue = (futilityBase > alpha) ? alpha : std::max(bestValue, futilityBase);
+                    bestValue = (ss->staticEval > alpha) ? alpha : std::max(bestValue, ss->staticEval);
                     continue;
                 }
             }
