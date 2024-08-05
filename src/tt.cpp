b5e5788ae4da6202eaeae1e035810da3fc29d7e3
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-08-05 12:34:00 +0200

    changed tt replacement scheme

--- src/tt.cpp
+++ src/tt.cpp
@@ -93,8 +93,8 @@ bool TTEntry::is_occupied() const { return bool(depth8); }
 void TTEntry::save(
   Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, uint8_t generation8) {
 
-    // Preserve the old ttmove if we don't have a new one
-    if (m || uint16_t(k) != key16)
+    // update the ttmove if we have a better one
+    if (uint16_t(k) != key16 || (m && (!move16 || depth8 > d - DEPTH_ENTRY_OFFSET)))
         move16 = m;
 
     // Overwrite less valuable entries (cheapest checks first)
