2fe9267b8db11778d13a0c871d28b274dba4f23f
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-08-09 16:28:03 +0200

    changed static eval to eval

--- src/search.cpp
+++ src/search.cpp
@@ -743,9 +743,9 @@ Value Search::Worker::search(
     // bigger than the previous static evaluation at our turn (if we were in
     // check at our previous move we go back until we weren't in check) and is
     // false otherwise. The improving flag is used in various pruning heuristics.
-    improving = ss->staticEval > (ss - 2)->staticEval;
+    improving = eval > (ss - 2)->staticEval;
 
-    opponentWorsening = ss->staticEval + (ss - 1)->staticEval > 2;
+    opponentWorsening = eval + (ss - 1)->staticEval > 2;
 
     // Step 7. Razoring (~1 Elo)
     // If eval is really low, check with qsearch if we can exceed alpha. If the
