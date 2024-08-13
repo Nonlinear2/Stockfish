9f9ddb299b4e1cfe9bc6d835ba021cb142cd9081
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-08-13 21:26:12 +0200

    Revert "see pruning attempt"
    This reverts commit 192ffe95e8025a937efd9ecb376e62e03c62b756.

--- src/search.cpp
+++ src/search.cpp
@@ -1566,10 +1566,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
                 // If static eval is much lower than alpha and move is
                 // not winning material, we can prune this move. (~2 Elo)
-                Value SeeValue = ss->staticEval - 375*(ss->staticEval > alpha);
-                if (!pos.see_ge(move, alpha - SeeValue))
+                if (!pos.see_ge(move, alpha - ss->staticEval))
                 {
-                    bestValue = (SeeValue > alpha) ? alpha : std::max(bestValue, SeeValue);
+                    bestValue = (ss->staticEval > alpha) ? alpha : std::max(bestValue, ss->staticEval);
                     continue;
                 }
             }
