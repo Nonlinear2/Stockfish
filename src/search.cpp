ed4d08f419bcfdb41e6a8557b067149f982f1a5e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-08-19 15:50:07 +0200

    Revert "take 2"
    This reverts commit 928758b76f34a57cfe66b98d3150728ce6e2bc29.

--- src/search.cpp
+++ src/search.cpp
@@ -1083,7 +1083,7 @@ moves_loop:  // When in check, search starts here
 
                 // If the ttMove is assumed to fail high over current beta (~7 Elo)
                 else if (ttData.value >= beta)
-                    extension = -2 - cutNode;
+                    extension = -3;
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high
                 // over current beta (~1 Elo)
