7d0ca9e112794ba82a19625f0f3cf7a689f9cf63
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-09-02 21:23:57 +0200

    allow futility pruning for moves that give checks in main search

--- src/search.cpp
+++ src/search.cpp
@@ -977,7 +977,7 @@ moves_loop:  // When in check, search starts here
                   thisThread->captureHistory[movedPiece][move.to_sq()][type_of(capturedPiece)];
 
                 // Futility pruning for captures (~2 Elo)
-                if (!givesCheck && lmrDepth < 7 && !ss->inCheck)
+                if (lmrDepth < 7 && !ss->inCheck)
                 {
                     Value futilityValue = ss->staticEval + 285 + 251 * lmrDepth
                                         + PieceValue[capturedPiece] + captHist / 7;
