9d58a9b0568e30808d1c8d5eb0c2817c43c7da09
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-09-06 10:04:18 +0200

    remove depth increase in singular search

--- src/search.cpp
+++ src/search.cpp
@@ -1062,8 +1062,6 @@ moves_loop:  // When in check, search starts here
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
-
-                    depth += ((!PvNode) && (depth < 16));
                 }
 
                 // Multi-cut pruning
