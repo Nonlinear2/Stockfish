2f4fadfab19f41793d500446113e34979187fe36
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-09-11 22:40:42 +0200

    reduce less if no cutoffs

--- src/search.cpp
+++ src/search.cpp
@@ -1302,7 +1302,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 14 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                        depth -= 2;
+                        depth -= 1 + (ss->cutoffCnt > 0);
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
