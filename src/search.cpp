26829917c11f9caf5903a6a86915cf64a6c3299f
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-09-16 19:46:34 +0200

    prune some checks

--- src/search.cpp
+++ src/search.cpp
@@ -1554,7 +1554,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us))
         {
             // Futility pruning and moveCount pruning (~10 Elo)
-            if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
+            if ((!givesCheck || (!pos.pieces(us, QUEEN, ROOK) && !pos.see_ge(move, 0))) && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
             {
                 if (moveCount > 2)
