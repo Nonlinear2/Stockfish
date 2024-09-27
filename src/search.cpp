246a9876d539eaa1d6d43566ef6b1666f4d160db
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-09-27 14:49:15 +0200

    decrease see threshold if non recapture

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,7 @@ moves_loop:  // When in check, search starts here
                 lmrDepth = std::max(lmrDepth, 0);
 
                 // Prune moves with negative SEE (~4 Elo)
-                if (!pos.see_ge(move, -24 * lmrDepth * lmrDepth))
+                if (!pos.see_ge(move, -(24 - 10*(priorCapture && bestMove.to_sq() == prevSq)) * lmrDepth * lmrDepth))
                     continue;
             }
         }
