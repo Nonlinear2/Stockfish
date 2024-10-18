a20acccc78eaeb403081448b3b08bdf116db3117
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-10-18 11:48:22 +0200

    use corrhist for qs tt evals

--- src/search.cpp
+++ src/search.cpp
@@ -745,7 +745,11 @@ Value Search::Worker::search(
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (ttData.value != VALUE_NONE
             && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)))
+        {
             eval = ttData.value;
+            if (ttData.depth == DEPTH_QS)
+                eval = (eval + to_corrected_static_eval(eval, *thisThread, pos, ss))/2;
+        }
     }
     else
     {
