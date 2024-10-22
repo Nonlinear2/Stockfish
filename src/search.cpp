09ee8038facfa224cdb7c9dd2dfd6b40f54b1852
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-10-22 16:03:35 +0200

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1104,7 +1104,7 @@ moves_loop:  // When in check, search starts here
                 // singular (multiple moves fail high), and we can prune the whole
                 // subtree by returning a softbound.
                 else if (value >= beta && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                    return value;
+                    return (4*value + beta)/5;
 
                 // Negative extensions
                 // If other moves failed high over (ttValue - margin) without the
