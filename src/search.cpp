f3ea7ed5944d0073280ad20007641e48c834d1cd
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-10-24 15:32:23 +0200

    small probcut tweak

--- src/search.cpp
+++ src/search.cpp
@@ -632,7 +632,7 @@ Value Search::Worker::search(
     ss->ttPv     = excludedMove ? ss->ttPv : PvNode || (ttHit && ttData.is_pv);
     ttCapture    = ttData.move && pos.capture_stage(ttData.move);
 
-    simplePosition = (ttData.bound == BOUND_EXACT && std::abs(ttData.eval - ttData.value) < 20);
+    simplePosition = std::abs(ttData.eval - ttData.value) < 20;
 
     // At this point, if excluded, skip straight to step 6, static eval. However,
     // to save indentation, we list the condition in all code between here and there.
@@ -929,7 +929,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea (~4 Elo)
-    probCutBeta = beta + 379 - 20*simplePosition;
+    probCutBeta = beta + 379 - 15*simplePosition;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)
