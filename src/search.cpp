0da73ca5c142bc7c5bd09f42a35e935edce6aa0e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-11-01 11:22:20 +0100

    take 5

--- src/search.cpp
+++ src/search.cpp
@@ -633,9 +633,11 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove
         && ttData.value != VALUE_NONE  // Can happen when !ttHit or when access race in probe()
-        && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER)))
+        && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
+        && ttData.depth > depth - (ttData.value <= beta) 
+                                - (ttData.eval > beta + 400 && ttData.value > beta + 100 && ttData.depth < 7))
     {
         // If ttMove is quiet, update move sorting heuristics on TT hit (~2 Elo)
         if (ttData.move && ttData.value >= beta)
