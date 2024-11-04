4c16136ba019b9fadee48ce43b2a52e0d2547986
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-11-04 20:04:30 +0100

    use tt to adjust static eval

--- src/search.cpp
+++ src/search.cpp
@@ -742,7 +742,10 @@ Value Search::Worker::search(
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (ttData.value != VALUE_NONE
             && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)))
+        {
             eval = ttData.value;
+            ss->staticEval = (4*ss->staticEval + eval)/5;
+        }
     }
     else
     {
