b05a3d06fd9b3896e0f146beea5f547e09d59923
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-11-15 15:22:25 +0100

    take 6

--- src/search.cpp
+++ src/search.cpp
@@ -740,7 +740,7 @@ Value Search::Worker::search(
 
         ss->staticEval = eval =
           to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos, ss);
-        eval = 2*eval / (2 + (eval > 0 && pos.has_just_repeated()));
+        eval = 3*eval / (3 + pos.has_just_repeated());
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (ttData.value != VALUE_NONE
             && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)))
@@ -752,7 +752,7 @@ Value Search::Worker::search(
           evaluate(networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us]);
         ss->staticEval = eval =
           to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos, ss);
-        eval = 2*eval / (2 + (eval > 0 && pos.has_just_repeated()));
+        eval = 3*eval / (3 + (pos.has_just_repeated()));
         // Static evaluation is saved as it was before adjustment by correction history
         ttWriter.write(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_UNSEARCHED, Move::none(),
                        unadjustedStaticEval, tt.generation());
