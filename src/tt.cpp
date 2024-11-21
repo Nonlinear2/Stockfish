305db104b4e60acc6e2a489c080cae2e7d9240e9
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-11-21 19:26:00 +0100

    take 6

--- src/tt.cpp
+++ src/tt.cpp
@@ -105,7 +105,7 @@ void TTEntry::save(
         assert(d < 256 + DEPTH_ENTRY_OFFSET);
 
         if (uint16_t(k) == key16 && d - DEPTH_ENTRY_OFFSET < depth8 && (genBound8 & 0x3) == BOUND_EXACT && b == BOUND_EXACT)
-            v = (v + value16)/2;
+            v = (v + (1 + (depth8 > d - DEPTH_ENTRY_OFFSET + 6)) * value16)/(2 + (depth8 > d - DEPTH_ENTRY_OFFSET + 6));
 
         key16     = uint16_t(k);
         depth8    = uint8_t(d - DEPTH_ENTRY_OFFSET);
