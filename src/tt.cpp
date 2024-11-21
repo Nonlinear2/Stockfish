ed23a1e6854db3c1be8c376c8d5fe962c2c63343
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-11-21 19:32:32 +0100

    take 6

--- src/tt.cpp
+++ src/tt.cpp
@@ -106,7 +106,7 @@ void TTEntry::save(
 
         // if the bounds of the new and old data are complementary, we can update the bound to be exact.
         if (uint16_t(k) == key16 && ((genBound8 & 0x3) ^ b) == BOUND_EXACT 
-            && value16 == v && depth8 == d - DEPTH_ENTRY_OFFSET && d < 5)
+            && value16 == v && depth8 >= d - DEPTH_ENTRY_OFFSET && d < 5)
             b = BOUND_EXACT;
 
         key16     = uint16_t(k);
