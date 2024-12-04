0b39ba3f5e45c71f7f1e8d8d0dd58710b89f9159
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-12-04 13:42:19 +0100

    keep exact bounds if same value

--- src/tt.cpp
+++ src/tt.cpp
@@ -97,9 +97,13 @@ void TTEntry::save(
     if (m || uint16_t(k) != key16)
         move16 = m;
 
+    // if the bounds of the new and old data are complementary, we can update the bound to be exact.
+    if (uint16_t(k) == key16 && (genBound8 & 0x3) == BOUND_EXACT 
+        && value16 == v && depth8 > d - DEPTH_ENTRY_OFFSET && d < 5)
+        b = BOUND_EXACT;
+
     // Overwrite less valuable entries (cheapest checks first)
-    if (b == BOUND_EXACT || uint16_t(k) != key16
-        || d - DEPTH_ENTRY_OFFSET + 2 * pv > depth8 - 5 + ((genBound8 & 0x3) == BOUND_EXACT)
+    if (b == BOUND_EXACT || uint16_t(k) != key16 || d - DEPTH_ENTRY_OFFSET + 2 * pv > depth8 - 4
         || relative_age(generation8))
     {
         assert(d > DEPTH_ENTRY_OFFSET);
