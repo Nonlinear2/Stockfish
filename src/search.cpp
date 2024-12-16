6a26bb6eea9820a8dba21c96ae0fe462484b3e1a
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-12-16 13:16:50 +0100

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1048,7 +1048,7 @@ moves_loop:  // When in check, search starts here
                 lmrDepth = std::max(lmrDepth, 0);
 
                 // Prune moves with negative SEE (~4 Elo)
-                if (!pos.see_ge(move, -25 * lmrDepth * lmrDepth))
+                if (!pos.see_ge(move, -23 * lmrDepth * lmrDepth + (alpha - futilityValue)))
                     continue;
             }
         }
