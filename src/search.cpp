d237d44346dc645d36b143a051ba94d80c02dd69
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-12-18 12:12:23 +0100

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1647,7 +1647,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
-            if (!pos.see_ge(move, -83 + 15*pvBadSee))
+            if (!pos.see_ge(move, -83 - PawnValue*pvBadSee))
                 continue;
         }
 
