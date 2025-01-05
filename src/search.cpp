42f36acde4c06b9f7ad68dfb52e9c7c1650367a5
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-05 11:35:35 +0100

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -68,7 +68,8 @@ namespace {
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool twiceImproving, bool oppWorsening) {
     Value futilityMult       = 109 - 27 * noTtCutNode;
     Value improvingDeduction = improving * futilityMult * 2;
-    Value twiceImprovingDeduction = twiceImproving * futilityMult / 6;
+    Value twiceImprovingDeduction = twiceImproving * futilityMult / 8;
+    dbg_extremes_of(twiceImprovingDeduction);
     Value worseningDeduction = oppWorsening * futilityMult / 3;
 
     return futilityMult * d - improvingDeduction - twiceImprovingDeduction - worseningDeduction;
