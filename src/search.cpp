30bf966fad857117855b32e549cdaff284bd61ec
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-12 00:38:35 +0100

    take 6

--- src/search.cpp
+++ src/search.cpp
@@ -1154,7 +1154,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r -= 985 + 40*(rootNode * std::min(depth, 15));
+            r -= 1024 + 400*rootNode;
 
         // These reduction adjustments have no proven non-linear scaling
 
