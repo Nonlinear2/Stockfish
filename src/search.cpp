1781e835fe906a8b14c5ee8a10e49b47df6cded6
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-13 17:35:02 +0100

    spsa tune 2

--- src/search.cpp
+++ src/search.cpp
@@ -744,6 +744,7 @@ Value Search::Worker::search(
     // Step 6. Static evaluation of the position
     Value      unadjustedStaticEval = VALUE_NONE;
     int staticEvalCorrectionValue, evalCorrectionValue;
+    bool eval_tt_adjusted = false;
     two_correction_value(*thisThread, pos, ss, staticEvalCorrectionValue, evalCorrectionValue);
     if (ss->inCheck)
     {
@@ -772,8 +773,10 @@ Value Search::Worker::search(
         eval = to_corrected_static_eval(unadjustedStaticEval, evalCorrectionValue);
 
         // ttValue can be used as a better position evaluation (~7 Elo)
-        if (is_valid(ttData.value)
-            && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)))
+        eval_tt_adjusted = (is_valid(ttData.value)
+            && (ttData.bound & (ttData.value > eval ? BOUND_LOWER : BOUND_UPPER)));
+        
+        if (eval_tt_adjusted)
             eval = ttData.value;
     }
     else
@@ -818,7 +821,7 @@ Value Search::Worker::search(
     if (!ss->ttPv && depth < 14
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
                - (ss - 1)->statScore / 310
-               + (ss->staticEval == eval) * (40 - std::abs(staticEvalCorrectionValue) / 131072)
+               + (!eval_tt_adjusted) * (40 - std::abs(evalCorrectionValue) / 131072)
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
         return beta + (eval - beta) / 3;
