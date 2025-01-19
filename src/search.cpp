1be8f8a1dd84089f412a69d5daf106c164d7c5b5
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-19 23:33:47 +0100

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -1201,7 +1201,7 @@ moves_loop:  // When in check, search starts here
             Depth d = std::max(
               1, std::min(newDepth - r / 1024, newDepth + !allNode + (PvNode && !bestMove)));
 
-            (ss + 1)->reduction = r - std::min((newDepth - r / 1024 - 1)*1024, 0);
+            (ss + 1)->reduction = r + std::min((newDepth - r / 1024 - 1)*1024, 0);
 
             value               = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
             (ss + 1)->reduction = 0;
