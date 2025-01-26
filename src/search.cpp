19e8a958e952c18c7960e215da6cdb7344b1fb11
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-26 02:14:32 +0100

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -811,7 +811,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 215, 7) + depth / 3 + 5;
+        Depth R = std::min(int(eval - beta) / 215, 7) + depth / 3 + 5 - (priorReduction >= 3);
 
         ss->currentMove                   = Move::null();
         ss->continuationHistory           = &thisThread->continuationHistory[0][0][NO_PIECE][0];
