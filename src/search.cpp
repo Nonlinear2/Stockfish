0f8e8ad7e9e04ad555b50058166e0b1e49b3e4ca
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-28 21:06:21 +0100

    take 5

--- src/search.cpp
+++ src/search.cpp
@@ -1631,7 +1631,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                        + (*contHist[1])[pos.moved_piece(move)][move.to_sq()]
                        + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)]
                                                 [move.to_sq()]
-                     <= 5228)
+                     <= 5228 - 3500*(move == ttData.move))
                 continue;
 
             // Do not search moves with bad enough SEE values
