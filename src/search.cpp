9b26be53b053519cb1ab1bd3b0ba985b468c86a2
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-29 23:13:43 +0100

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r -= 1018;
 
-        if ((ss - 1)->extension <= -3 && !opponentWorsening)
+        if ((ss - 1)->extension <= -3 && improving)
             r -= 1024;
 
         // These reduction adjustments have no proven non-linear scaling
