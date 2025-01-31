03016c68149eaa1139cd453f261de7f5c2865947
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-31 11:58:16 +0100

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1181,6 +1181,9 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 1451 / 16384;
 
+        if (is_valid(ttData.value) && (cutNode != (ttData.value >= beta)))
+            r -= 600;
+
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
