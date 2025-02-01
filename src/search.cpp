dc6d567a6948fe71b62524bc2fa972b253a74641
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-02-01 01:21:35 +0100

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -667,9 +667,14 @@ Value Search::Worker::search(
 
         if (ttData.move && ttData.value < alpha && !priorCapture && prevSq != SQ_NONE)
         {
+            const int scaledBonus = stat_bonus(depth) *
+                (118 * (depth > 5) + 37 * !allNode
+                + 169 * ((ss - 1)->moveCount > 8) + 80 * (ss - 1)->isTTMove);
+
             update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
-                                            stat_bonus(depth + 1)/5);
+                                            scaledBonus * 50 / 32768);
         }
+
 
         // Partial workaround for the graph history interaction problem
         // For high rule50 counts don't produce transposition table cutoffs.
