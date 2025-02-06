4b78e1400646158708cb1976564f1424f4f67fd0
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-02-06 17:16:20 +0100

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1618,7 +1618,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 // much lower than alpha, we can prune this move.
                 if (futilityValue <= alpha)
                 {
-                    bestValue = std::max(bestValue, (4*futilityValue + alpha)/5);
+                    bestValue = std::max(bestValue, (3*futilityValue + alpha)/4);
                     continue;
                 }
 
