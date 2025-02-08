0027fe4240316bc42d667b45dbe4e7ec2c75dc68
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-02-09 00:02:23 +0100

    take 7

--- src/search.cpp
+++ src/search.cpp
@@ -1223,7 +1223,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 41 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 9 + 40 * (r % 1024 >= 1000);
+                const bool doShallowerSearch = value < bestValue + 9 + 40 * (r % 1024 >= 1000 && depth < 7);
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
