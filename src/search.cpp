0a2896a3afbec1f27a06b380154318fa846cc6ec
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-02-09 18:10:40 +0100

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -805,7 +805,7 @@ Value Search::Worker::search(
 
     improving = ss->staticEval > (ss - 2)->staticEval;
 
-    improving |= !excludedMove && ttData.depth > std::max(depth - 2, DEPTH_QS)
+    improving |= !excludedMove && ttData.depth > std::max(depth - 2, 0)
         && is_valid(ttData.value) && ttData.value > alpha
         && (ttData.bound & BOUND_LOWER);
 
