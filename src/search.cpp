8b87e402125a9cf2bfaa00b7a0dfd93b65b26036
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-02-09 22:53:35 +0100

    take 7

--- src/search.cpp
+++ src/search.cpp
@@ -805,8 +805,8 @@ Value Search::Worker::search(
 
     improving = ss->staticEval > (ss - 2)->staticEval;
 
-    improving |= !excludedMove && ttData.depth > depth - 1
-        && is_valid(ttData.value) && ttData.value > alpha + 50
+    improving |= !excludedMove && ttData.depth > depth
+        && is_valid(ttData.value) && ttData.value > alpha
         && (ttData.bound & BOUND_LOWER);
 
     opponentWorsening = ss->staticEval + (ss - 1)->staticEval > 5;
