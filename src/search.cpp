4297d6cb950d9e318b0cb53f30cc002e3089d7a1
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-03-02 12:51:53 +0100

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1476,8 +1476,8 @@ moves_loop:  // When in check, search starts here
         && ((bestValue < ss->staticEval && bestValue < beta)  // negative correction & no fail high
             || (bestValue > ss->staticEval && bestMove)))     // positive correction & no fail low
     {
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / (8 - PvNode),
-                                -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / (8 - 2*PvNode),
+                                -CORRECTION_HISTORY_LIMIT / (4 - PvNode), CORRECTION_HISTORY_LIMIT / (4 - PvNode));
         update_correction_history(pos, ss, *thisThread, bonus);
     }
 
