a976668586c044a7575daa0b350d6878aa437890
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-03-28 17:11:43 +0100

    take 1

--- src/tt.cpp
+++ src/tt.cpp
@@ -94,7 +94,7 @@ void TTEntry::save(
   Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, uint8_t generation8) {
 
     // Preserve the old ttmove if we don't have a new one
-    if (m || uint16_t(k) != key16)
+    if (m && !move16)
         move16 = m;
 
     // Overwrite less valuable entries (cheapest checks first)
@@ -107,6 +107,7 @@ void TTEntry::save(
         key16     = uint16_t(k);
         depth8    = uint8_t(d - DEPTH_ENTRY_OFFSET);
         genBound8 = uint8_t(generation8 | uint8_t(pv) << 2 | b);
+        move16 = m;
         value16   = int16_t(v);
         eval16    = int16_t(ev);
     }
