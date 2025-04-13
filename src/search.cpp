d262eea91e24eda53fe9d0378dd6f2e37aa14e17
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-04-14 00:36:54 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1285,7 +1285,7 @@ moves_loop:  // When in check, search starts here
         {
             // Increase reduction if ttMove is not present
             if (!ttData.move)
-                r += 1100 + 550 * cutNode;
+                r += 1100 + 950 * cutNode;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
