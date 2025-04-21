1373e61816e7fbbd0678a7552f6292e4b16e437c
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-04-21 02:59:26 +0200

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -1299,7 +1299,7 @@ moves_loop:  // When in check, search starts here
                 r += 1156;
 
             else if (newDepth == 1)
-                r -= 400;
+                r -= 650;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
