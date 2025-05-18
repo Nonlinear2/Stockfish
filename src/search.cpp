f2766c6e6d6b8412b415d0e5340d335354af4f4b
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-05-18 19:25:31 +0200

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -675,28 +675,50 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
-        && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
-        && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
-        && (cutNode == (ttData.value >= beta) || depth > 5))
+    if (!PvNode && !excludedMove)
     {
-        // If ttMove is quiet, update move sorting heuristics on TT hit
-        if (ttData.move && ttData.value >= beta)
+        if (ttData.depth > depth - (ttData.value <= beta)
+            && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
+            && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
+            && (cutNode == (ttData.value >= beta) || depth > 5))
         {
-            // Bonus for a quiet ttMove that fails high
-            if (!ttCapture)
-                update_quiet_histories(pos, ss, *this, ttData.move,
-                                       std::min(125 * depth - 77, 1157));
+            // If ttMove is quiet, update move sorting heuristics on TT hit
+            if (ttData.move && ttData.value >= beta)
+            {
+                // Bonus for a quiet ttMove that fails high
+                if (!ttCapture)
+                    update_quiet_histories(pos, ss, *this, ttData.move,
+                                        std::min(125 * depth - 77, 1157));
 
-            // Extra penalty for early quiet moves of the previous ply
-            if (prevSq != SQ_NONE && (ss - 1)->moveCount <= 3 && !priorCapture)
-                update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq, -2301);
+                // Extra penalty for early quiet moves of the previous ply
+                if (prevSq != SQ_NONE && (ss - 1)->moveCount <= 3 && !priorCapture)
+                    update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq, -2301);
+            }
+
+            // Partial workaround for the graph history interaction problem
+            // For high rule50 counts don't produce transposition table cutoffs.
+            if (pos.rule50_count() < 90)
+                return ttData.value;
         }
+        else if (cutNode && !ss->inCheck && (ss - 1)->currentMove != Move::null()
+                 && pos.non_pawn_material(us) && !is_loss(beta) && depth < 6) // look at an entry with other stm
+        {
+            // there are surely better ways to do that
+            do_null_move(pos, st);
 
-        // Partial workaround for the graph history interaction problem
-        // For high rule50 counts don't produce transposition table cutoffs.
-        if (pos.rule50_count() < 90)
-            return ttData.value;
+            Key posKeyNull = pos.key();
+            auto [ttHitNull, ttDataNull, ttWriterNull] = tt.probe(posKeyNull);
+            ttDataNull.value = ttHitNull
+                ? value_from_tt(ttDataNull.value, ss->ply, pos.rule50_count())
+                : VALUE_NONE;
+
+            undo_null_move(pos);
+            
+            if (ttDataNull.depth >= depth && is_valid(ttDataNull.value)
+                && !is_win(-ttDataNull.value) && -ttDataNull.value >= beta + 100
+                && (ttDataNull.bound & BOUND_UPPER))
+                return -ttDataNull.value;
+        }
     }
 
     // Step 5. Tablebases probe
