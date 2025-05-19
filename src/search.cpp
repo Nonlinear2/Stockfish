7301c53f4342e393e67a86cb704fb3c6771bd216
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-05-19 03:04:34 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop:  // When in check, search starts here
             r += 1210 + (depth < 8) * 963;
 
         if (priorCapture && !capture && !givesCheck)
-            r += 550;
+            r += 200;
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
