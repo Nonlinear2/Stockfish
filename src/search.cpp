9c1814b3724e62b6c398c1025bf0b5f8ffcb63f2
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-05-19 03:17:04 +0200

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -670,7 +670,7 @@ Value Search::Worker::search(
     ttData.value = ttHit ? value_from_tt(ttData.value, ss->ply, pos.rule50_count()) : VALUE_NONE;
     ss->ttPv     = excludedMove ? ss->ttPv : PvNode || (ttHit && ttData.is_pv);
     ttCapture    = ttData.move && pos.capture_stage(ttData.move);
-    ttGivesCheck = ttData.move && pos.gives_check(ttData.move);
+    ttGivesCheck = ttData.move && pos.moved_piece(ttData.move) != NO_PIECE && pos.gives_check(ttData.move);
 
     // At this point, if excluded, skip straight to step 6, static eval. However,
     // to save indentation, we list the condition in all code between here and there.
