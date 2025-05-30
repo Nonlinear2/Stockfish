1020828592103b9db9afb8408f7bbf3cc277cb5e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-05-30 18:10:31 +0200

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -710,7 +710,11 @@ Value Search::Worker::search(
                     return ttData.value;
 
                 if (ttData.value >= beta && -ttDataNext.value >= beta)
-                    return ttData.value;
+                {
+                    int next_depth = std::max(0, ttDataNext.depth);
+                    return (ttData.depth * ttData.value - next_depth * ttDataNext.value)
+                        / (ttData.depth + next_depth);
+                }
 
                 if (ttData.value <= alpha && -ttDataNext.value <= alpha)
                     return ttData.value;
