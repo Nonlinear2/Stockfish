1bd39a1b90760011d1cb915a94c90eaf4e62aefc
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-06-03 02:28:12 +0200

    take 1

--- src/search.cpp
+++ src/search.cpp
@@ -823,8 +823,8 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 1 && depth >= 2
-        && ((ss - 1)->currentMove == Move::null() || ss->staticEval + (ss - 1)->staticEval > 175))
+    if (priorReduction >= 1 && depth >= 2 && (ss - 1)->currentMove != Move::null()
+        && ss->staticEval + (ss - 1)->staticEval > 175)
         depth--;
 
     // Step 7. Razoring
