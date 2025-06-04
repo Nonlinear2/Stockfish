f887c8977d4df8cff3d3c668e63c858f0e30af01
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-06-04 23:27:05 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -710,7 +710,7 @@ Value Search::Worker::search(
                     return ttData.value;
                 else if (ttData.value >= beta && ttDataNext.move && !pos.capture_stage(ttDataNext.move))
                     // bonus for next move
-                    update_quiet_histories(pos, ss, *this, ttDataNext.move, std::min(25 * depth - 15, 250));
+                    update_quiet_histories(pos, ss, *this, ttDataNext.move, std::min(67 * depth - 38, 740));
             }
             else
                 return ttData.value;
