bcb1b3b2e20f66e5f5be7168a223d16bf77fc4b6
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-06-18 00:04:13 +0200

    Revert "simplify tt move access"
    This reverts commit 854a8f81ccdb4bec1b8c3aa27db6d5017bf4ced6.

--- src/search.cpp
+++ src/search.cpp
@@ -663,7 +663,8 @@ Value Search::Worker::search(
     // Need further processing of the saved data
     ss->ttHit    = ttHit;
     ttData.move  = rootNode ? thisThread->rootMoves[thisThread->pvIdx].pv[0]
-                 : ttData.move;
+                 : ttHit    ? ttData.move
+                            : Move::none();
     ttData.value = ttHit ? value_from_tt(ttData.value, ss->ply, pos.rule50_count()) : VALUE_NONE;
     ss->ttPv     = excludedMove ? ss->ttPv : PvNode || (ttHit && ttData.is_pv);
     ttCapture    = ttData.move && pos.capture_stage(ttData.move);
@@ -1550,6 +1551,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
     auto [ttHit, ttData, ttWriter] = tt.probe(posKey);
     // Need further processing of the saved data
     ss->ttHit    = ttHit;
+    ttData.move  = ttHit ? ttData.move : Move::none();
     ttData.value = ttHit ? value_from_tt(ttData.value, ss->ply, pos.rule50_count()) : VALUE_NONE;
     pvHit        = ttHit && ttData.is_pv;
 
