13cc27ff2f37d73c8122ee58915163d55c580469
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-07-02 23:21:58 +0200

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1137,7 +1137,7 @@ moves_loop:  // When in check, search starts here
                 int doubleMargin = -4 + 244 * PvNode - 206 * !ttCapture - corrValAdj
                                  - 997 * ttMoveHistory / 131072
                                  - (ss->ply > thisThread->rootDepth) * 47
-                                 - 20 * (!pos.see_ge(move, 0));
+                                 - 20 * (!pos.see_ge(move, 0) && depth < 8);
 
                 int tripleMargin = 84 + 269 * PvNode - 253 * !ttCapture + 91 * ss->ttPv - corrValAdj
                                  - (ss->ply * 2 > thisThread->rootDepth * 3) * 54;
