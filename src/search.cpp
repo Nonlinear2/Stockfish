c37a40c55cec4852eb805bfdb4259ed780d72633
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-07-27 14:37:47 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1397,7 +1397,7 @@ moves_loop:  // When in check, search starts here
         bestValue = (bestValue * depth + beta) / (depth + 1);
 
     if (!moveCount)
-        bestValue = excludedMove ? alpha - 225 : ss->inCheck ? mated_in(ss->ply) : VALUE_DRAW;
+        bestValue = excludedMove ? beta : ss->inCheck ? mated_in(ss->ply) : VALUE_DRAW;
 
     // If there is a move that produces search value greater than alpha,
     // we update the stats of searched moves.
