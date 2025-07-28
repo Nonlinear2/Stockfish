d4a2589cfe9e454d62f948f0a326cfe960d51bca
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-07-28 22:04:45 +0200

    take 5

--- src/search.cpp
+++ src/search.cpp
@@ -1616,7 +1616,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2 && !pos.see_ge(move, 255))
+                if (moveCount > 2 && !pos.see_ge(move, 235))
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
