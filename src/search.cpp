c2922e801ad647384d3a16124f14c1ec9b1411a8
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-08 14:18:46 +0200

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1545,7 +1545,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         bestValue = futilityBase = -VALUE_INFINITE;
     else
     {
-        const auto correctionValue = (8 + 2*(ttData.move && pos.capture_stage(ttData.move))) * correction_value(*this, pos, ss) / 9;;
+        const auto correctionValue = (9 + 4*(ttData.move && pos.capture_stage(ttData.move))) * correction_value(*this, pos, ss) / 10;
 
         if (ss->ttHit)
         {
