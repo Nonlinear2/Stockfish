0c69c34e7de5b30d1a96b56b49c457b773c5b34d
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-18 00:54:05 +0200

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -876,7 +876,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && !is_win(nullValue))
         {
             if (nmpMinPly || depth < 16)
-                return (3*nullValue + beta) / 4;
+                return (4*nullValue + beta) / 5;
 
             assert(!nmpMinPly);  // Recursive verification is not allowed
 
@@ -889,7 +889,7 @@ Value Search::Worker::search(
             nmpMinPly = 0;
 
             if (v >= beta)
-                return (3*nullValue + beta) / 4;
+                return (4*nullValue + beta) / 5;
         }
     }
 
