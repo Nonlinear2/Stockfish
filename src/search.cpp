dcf0814de4c62b84cef5b74181cc741bbafff0ac
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-18 01:25:51 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1164,8 +1164,8 @@ moves_loop:  // When in check, search starts here
         }
 
         // this condition needs to be evaluated before the move is pushed
-        if (pos.rule50_count() > 70 && (capture || type_of(movedPiece) == PAWN))
-            r -= 750;
+        if (pos.rule50_count() > 80 && (capture || type_of(movedPiece) == PAWN))
+            r -= 1024;
 
         // Step 16. Make the move
         do_move(pos, move, st, givesCheck, ss);
