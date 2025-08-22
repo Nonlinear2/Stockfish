d0593298113503d969b739b3b30a799da6af82c8
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-23 01:31:50 +0200

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -680,7 +680,7 @@ Value Search::Worker::search(
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 5)
-        && (depth > 8
+        && (depth < 8
             || ttData.move == Move::none() 
             || pos.rule50_count() < 80 
             || (!ttCapture && type_of(pos.moved_piece(ttData.move)) != PAWN)))
