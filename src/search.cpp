4f26ec33661430a0104f43dede0b82e880f8b6cd
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-08-27 12:32:59 +0200

    take 6

--- src/search.cpp
+++ src/search.cpp
@@ -1651,7 +1651,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
 
         // Step 7. Make and search the move
         do_move(pos, move, st, givesCheck, ss);
-        if (PvNode && move == ttData.move && ttData.value > alpha && ttData.depth >= DEPTH_QS)
+        if (PvNode && ss->inCheck && ttData.value > alpha && ttData.depth >= DEPTH_QS)
         {
             (ss + 1)->pv = pv;
             (ss + 1)->pv[0] = Move::none();
