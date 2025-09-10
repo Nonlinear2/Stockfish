6e556c490c45805b2918b0dfdff5430502131d6c
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-09-10 22:43:11 +0200

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 3094 + 1056 * !ttData.move + 550 * !nextTTMove;
+            r += 3094 + 1056 * !ttData.move + 800 * !nextTTMove;
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
