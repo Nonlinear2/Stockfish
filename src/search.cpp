bbdd37a67f9f8af3f80ce3932c828b12e10dbd1e
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-09-10 21:26:46 +0200

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -1185,7 +1185,7 @@ moves_loop:  // When in check, search starts here
 
         // These reduction adjustments have no proven non-linear scaling
 
-        r += 543;  // Base reduction offset to compensate for other tweaks
+        r += 600;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 66;
         r -= std::abs(correctionValue) / 30450;
 
