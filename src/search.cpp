375ebb23e5f491b422bcd389c93217c96e4c39d7
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-09-25 23:43:18 +0200

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -92,7 +92,7 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
 // Add correctionHistory value to raw staticEval and guarantee evaluation
 // does not hit the tablebase range.
 Value to_corrected_static_eval(const Value v, const int cv, const int simpleEval) {
-    return std::clamp(v + cv / (131572 - 1000 * (std::abs(simpleEval) > 962)),
+    return std::clamp(v + cv / (132572 - 3000 * (std::abs(simpleEval) > 962)),
         VALUE_TB_LOSS_IN_MAX_PLY + 1, 
         VALUE_TB_WIN_IN_MAX_PLY - 1);
 }
